a879b665aa196fa84873282c6d3d0ad903e61651
Andrea Clark Sevilla <33915683+andreaoclarksevilla@users.noreply.github.com>
2019-09-10 15:39:16 -0400

    Add files via upload

---
+++ get_glove.py
@@ -0,0 +1,24 @@
+from flair.embeddings import Sentence, WordEmbeddings
+import numpy as np
+import pickle
+import tqdm as tqdm
+
+
+def get_glove(word2idx):
+    embedding_matrix = np.zeros((vocabulary_size + 1, 100))
+    glove = WordEmbeddings('glove')
+    for word, index in tqdm(word2idx.items()):
+        try:
+            word_ = Sentence(word)
+            glove.embed(word_)
+            embedding_vector = word_[0].embedding.cpu().detach().numpy()
+            embedding_matrix[index] = embedding_vector
+        except KeyError:
+            embedding_matrix[index] = np.random.normal(0, np.sqrt(0.25), 100)
+    
+    return embedding_matrix
+
+# save embeddings
+pickle_out = open("glove_100.pickle","wb")
+pickle.dump(glove_embeddings, pickle_out)
+pickle_out.close()
